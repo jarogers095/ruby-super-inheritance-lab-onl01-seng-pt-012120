fcf01354b41680eace894edc6a76ad1fb32a72b8
James Rogers <jarogers095@gmail.com>
2020-02-17 23:33:52 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ require "./lib/student.rb"
 class ChattyStudent < Student
   def hello()
     super()
-    puts "Howare you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
   
   def raise_hand()
